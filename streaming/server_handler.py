596e1438992258b6e57aa1ffae36ba8b507c9b41
schauppi <46676413+schauppi@users.noreply.github.com>
2022-12-09 11:23:48 +0100

    client server

--- streaming/server_handler.py
+++ streaming/server_handler.py
@@ -18,7 +18,7 @@ class JetsonNanoServer():
                 server_socket.bind(('', port))
                 #server_socket.listen(5)
 
-                cap = cv2.VideoCapture(url)
+                cap = cv2.VideoCapture(url, cv2.CAP_DSHOW)
                 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
                 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
