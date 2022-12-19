dc72b8130184795baf857cb766421c912c6cb9e5
schauppi <46676413+schauppi@users.noreply.github.com>
2022-12-19 15:06:49 +0100

    yolov5 detection class

--- object_detection_v5/object_detection_handler_yolov5.py
+++ object_detection_v5/object_detection_handler_yolov5.py
@@ -5,16 +5,54 @@ sys.path.append('../')
 import torch
 import numpy as np
 import random
-
 import cv2
 
-from torchsummary import summary
-
 from handler.image_preparation_handler import ImagePreparation
+from utils.general import non_max_suppression
+from handler.plot_handler import HandlePlot
 
-device = "mps"
+device = "cuda"
 
-model = torch.hub.load('', 'custom', path='model_weights/yolov5s.pt', source='local')
+class ObjectDetectionv5():
+
+        def __init__(self) -> None:
+                try:
+                        self.model = torch.hub.load('', 'custom', path='model_weights/yolov5s.pt', source='local')
+                        self.model.to(device)
+                        self.model.eval()
+                        #warmup
+                        image = torch.rand(1, 3, 640, 640).to(device)
+                        with torch.inference_mode():
+                                self.model(image)
+                        print("Object Detection Model loaded sucessfully")
+                except:
+                        print("Failed loading Object Detection Model")
+
+        def predict(self, image):
+                with torch.inference_mode():
+                        pred = self.model(image)
+
+                prediction = non_max_suppression(pred)[0].to(device)
+
+                if len(prediction) > 0:
+                        return prediction
+                else:
+                        return None
+
+        
+        def detect_objects(self, frame):
+                image, original_height, original_width = ImagePreparation.prepare_image(frame, device=device, model="yolov5")
+                prediction = self.predict(image)
+                if prediction is not None:
+                        prediction = ImagePreparation.resize_object_detection_prediction_output(prediction, original_height, original_width)
+                        image_with_boxes, person_counter = HandlePlot.plot_bounding_boxes(prediction, frame)
+                        return image_with_boxes, person_counter
+                else:
+                        return frame, 0
+
+
+
+"""model = torch.hub.load('', 'custom', path='model_weights/yolov5s.pt', source='local')
 model.to(device)
 print("loaded")
 
@@ -23,11 +61,20 @@ cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
 
-    image_test, _, _ = ImagePreparation.prepare_image(frame, device=device, model="yolov5")
+    image_test, height, width = ImagePreparation.prepare_image(frame, device=device, model="yolov5")
+    print(image_test.shape)
+    tensor_image = torch.rand(1, 3, 640, 640).to(device)
+    print(tensor_image.shape)
 
     results = model(image_test)
 
-    cv2.imshow('Recieving Stream...', frame)
+    prediction = non_max_suppression(results)[0].to(device)
+
+    prediction = ImagePreparation.resize_object_detection_prediction_output(prediction, height, width)
+    image_with_boxes, person_counter = HandlePlot.plot_bounding_boxes(prediction, frame)
+
+
+    cv2.imshow('Recieving Stream...', image_with_boxes)
 
     # check if the user pressed the "q" key
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -35,5 +82,5 @@ while True:
 
     # close the window
     cv2.destroyAllWindows()
-
+"""
 
